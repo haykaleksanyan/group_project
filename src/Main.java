c225141ac859e08849edd8dee047c090b05cf1e9
Karine <karine_papoyan@edu.aua.am>
2025-04-26 12:20:11 +0400

    Ship class and Main are updated Player class is updated

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player("Player1");
-        Player player2 = new Player("Player2");
-        player1.selfBoard.fillRandom();
-        player2.selfBoard.fillRandom();
+
     }
 }
