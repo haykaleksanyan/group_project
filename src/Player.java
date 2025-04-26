b10cafdcee1662be0231130b62f8a34eced78166
Karine <karine_papoyan@edu.aua.am>
2025-04-26 12:16:19 +0400

    Updated Ship class and Main Added Player class

--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,70 @@
+import java.util.ArrayList;
+
 public class Player {
-    public Board selfBoard;
-    public Board opponentBoard;
-    public String nickname;
-    public boolean win;
-    public Coordinate[] hitsOnOpponent;
+    private String name;
+    private Board board;
+    private int budget;
+    private ArrayList<Ship> ships;
+    private Weapon basicWeapon;
+    private ArrayList<Weapon> inventory;
 
-    public Player(String nickname) {
-        selfBoard = new Board("Player board");
-        opponentBoard = new Board("Opponent's board");
-        this.nickname = nickname;
-        win = false;
-        hitsOnOpponent = new Coordinate[Board.BOARD_SIZE * Board.BOARD_SIZE];
+    public Player(String name, int budget) {
+        this.board = new Board();
+        this.ships = new ArrayList<>();
+        this.inventory = new ArrayList<>();
+        this.name = name;
+        this.budget = budget;
+        basicWeapon = new BasicAttack();
     }
-    public void registerHit(int col, int row){
 
+    public boolean placeShip(int row, int col) {
+        if (board.placeShip(row, col)) {
+            Ship ship = new Ship(row, col);
+            ships.add(ship);
+            return true;
+        }
+        return false;
     }
+
+    public boolean buyWeapon(String weaponName) {
+        Weapon weapon = WeaponFactory.createWeapon(weaponName);
+        if (budget >= weapon.getPrice()) {
+            budget -= weapon.getPrice();
+            inventory.add(weapon);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean useWeapon(String weaponName, Player opponent, int row, int col) {
+        boolean hasWeapon = false;
+        int indexOfWeapon = 0;
+        for (Weapon w : inventory) {
+            if (w.equals(weaponName)) {
+                hasWeapon = true;
+                indexOfWeapon = inventory.indexOf(w);
+                break;
+            }
+        }
+        if (hasWeapon) {
+            inventory.get(indexOfWeapon).use(this, opponent, row, col);
+            inventory.remove(indexOfWeapon);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean basicAttack(Player opponent, int row, int col) {
+        basicWeapon.use(this, opponent, row, col);
+        return true;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public int getRemainingShips(){
+        return ships.size();
+    }
+
 }
--- src/Ship.java
+++ src/Ship.java
@@ -1,85 +1,17 @@
 public class Ship {
-    private int size; // size of the ship
-    private int startRow;
-    private int startCol;
-    private boolean isHorizontal;
-    private boolean[] hits;
+    private int x;
+    private int y;
 
-
-    public Ship(int size, int startRow, int startCol, boolean isHorizontal) {
-        this.size = size;
-        this.startRow = startRow;
-        this.startCol = startCol;
-        this.isHorizontal = isHorizontal;
-        this.hits = new boolean[size];
+    public Ship(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public int getSize() {
-        return size;
+    public int getX() {
+        return x;
     }
 
-    public int getStartRow() {
-        return startRow;
+    public int getY() {
+        return y;
     }
-
-    public int getStartCol() {
-        return startCol;
-    }
-
-    public boolean getisHorizontal() {
-        return isHorizontal;
-    }
-
-    public boolean[] getHits() {
-        return hits;
-    }
-
-    public Ship(Ship that) {
-        this.size = that.size;
-        this.startRow = that.startRow;
-        this.startCol = that.startCol;
-        this.isHorizontal = that.isHorizontal;
-
-        this.hits = new boolean[size];
-        for (int i = 0; i < size; i++) {
-            this.hits[i] = that.hits[i];
-        }
-    }
-
-    public boolean isHit(int row, int col) {
-        if (isHorizontal) {
-           return hits[col - startCol];
-        } else {
-           return hits[row - startRow];
-        }
-    }
-
-    public boolean isSunk() {
-        for (int i = 0; i < size; i++) {
-            if (!hits[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean isOcuupied(int row, int col) {
-        if (isHorizontal) {
-            return (row == startRow) && (col >= startCol) && (col < startCol + size);
-        } else {
-            return (col == startCol) && (row >= startRow) && (row < startRow + size);
-        }
-    }
-
-    public void registerHit(int row, int col) {
-        if (isOcuupied(row, col)) {
-            if (isHorizontal) {
-                hits[col - startCol] = true;
-            } else {
-                hits[row - startRow] = true;
-            }
-        }
-    }
-
-
 }
